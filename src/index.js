a2027cc79d401bab19cc0023eccb7b7f28138575
jack-king1 <jackking.gm97@gmail.com>
2025-02-07 01:04:56 +0000

    aded styling and js modules for tabbed pages

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import {loadMenu, loadHome, loadContact, loadAbout} from "./page-load.js"
+
+const pageArray = {};
+let contentDiv = "";
+
+
+function changePage(pageID){
+    contentDiv.innerHTML = "";
+    contentDiv.innerHTML = pageArray[pageID];
+}
+
+function getCotnentDiv(){
+    contentDiv= document.getElementById("content");
+}
+
+function initPages(){
+    pageArray[0] = loadHome();
+    pageArray[1] = loadMenu();
+    pageArray[2] = loadAbout();
+    pageArray[3] = loadContact();
+    console.log(pageArray);
+}
+
+function addEventListenerToNavBtns(){
+    const navBtns = document.getElementsByClassName("nav-btn");
+    console.log(navBtns);
+    for(let btn of navBtns){
+        btn.addEventListener("click", () => changePage(btn.getAttribute("btn-id")));
+
+    }
+}
+
+initPages();
+getCotnentDiv();
+addEventListenerToNavBtns();
---
+++ src/page-load.js
@@ -0,0 +1,56 @@
+function loadHome(){
+    let homeContent = `<div id="home">
+            <div>
+                <h3>Welcome!</h3>
+            </div>
+            <div>
+                <p>We are Beaned, we are a coffee house in Exeter. With naturally
+                    curated beans and customer service to match we are the perfect
+                    place for a weekend catchup or an office hangout!
+                </p>
+            </div>
+            <div><button>See Menu</button></div>
+        </div>`;
+    return homeContent;
+}
+
+function loadAbout(){
+    let aboutContent = `<div id="about">
+    <div>
+        <h3>About</h3>
+    </div>
+    <div>
+        <p>
+        </p>
+    </div>
+</div>`;
+return aboutContent;
+}
+
+function loadMenu(){
+    let menuContent = `<div id="menu">
+    <div>
+        <h3>Menu</h3>
+    </div>
+    <div>
+        <p>
+        </p>
+    </div>
+</div>`;
+return menuContent;
+}
+
+function loadContact(){
+    let contactContent = `<div id="contact">
+    <div>
+        <h3>Contact</h3>
+    </div>
+    <div>
+        <p>
+        </p>
+    </div>
+</div>`;
+return contactContent;
+}
+
+export {loadAbout, loadHome, loadMenu, loadContact}
